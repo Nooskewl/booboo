f9753a27479112bec330a20ac2b1d11eae3de697
Trent Gamblin <skinmarquee@gmail.com>
2023-12-16 05:02:26 -0700

    Use 480p on Pi

--- src/game_launcher.cpp
+++ src/game_launcher.cpp
@@ -520,8 +520,8 @@ bool start()
 		win_h = 1080;
 	}
 #else
-	int win_w = 960;
-	int win_h = 540;
+	int win_w = 854;
+	int win_h = 480;
 #endif
 
 	//if (shim::start_all(0, 0, false, desktop_resolution.w, desktop_resolution.h) == false) {
