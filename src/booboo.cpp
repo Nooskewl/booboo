a254d2d293e02ea4385811bf32063b9713f3497c
Trent Gamblin <trent@nooskewl.com>
2025-11-24 15:14:05 -0700

    Clear maps on new 'var' keyword like vectors

--- src/booboo.cpp
+++ src/booboo.cpp
@@ -2122,6 +2122,7 @@ bool corefunc_var(Program *prg, const std::vector<Token> &v)
 	MIN_ARGS(1)
 	for (size_t i = 0; i < v.size(); i++) {
 		prg->variables[v[i].i].v.clear();
+		prg->variables[v[i].i].m.clear();
 	}
 	return true;
 }
